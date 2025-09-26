42a996dec136304bdc8ab378be8dc7c7cd9a424c
RajatTheDev <RajatTheDev@users.noreply.github.com>
2025-09-26 22:42:24 +0530

    feat: add Prettier formatting webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,46 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
-        app: "./src/index.js",
+        app: './src/index.js',
     },
     output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    devtool: "eval-source-map",
+    devtool: 'eval-source-map',
     devServer: {
-        static: "./dist",
-        watchFiles: ["./src/template.html"],
+        static: './dist',
+        watchFiles: ['./src/template.html'],
         hot: true,
         open: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/template.html",
-            title: "Production",
+            template: './src/template.html',
+            title: 'Production',
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.html$/i,
-                loader: "html-loader",
+                loader: 'html-loader',
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
+                type: 'asset/resource',
             },
             {
                 test: /\.(woff|woff2|eof|ttf|otf)$/i,
-                type: "asset/resource",
+                type: 'asset/resource',
             },
         ],
     },
-};
+}
